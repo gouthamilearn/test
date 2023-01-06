8762a6fc9e92b11467fdcf3345df7f4c1bc46cc0
gouthamilearn <122092136+gouthamilearn@users.noreply.github.com>
2023-01-06 16:20:14 +0530

    Add files via upload

---
+++ zipcodes/src/main/java/com/assignments/controller/ZipcodeController.java
@@ -0,0 +1,26 @@
+package com.assignments.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.assignments.resource.Address;
+
+@RestController
+public class ZipcodeController {
+
+	@GetMapping("/zipcode")
+	public Address getAddressByZipcode(@PathVariable("zipcode") int zipcode) {
+		Map<Integer, Address> address = new HashMap<>();
+		address.put(600079, new Address("Chennai", "TamilNadu", "India"));
+		address.put(560103, new Address("Bangalore", "Karnataka", "India"));
+		address.put(621001, new Address("Kumbakonam","TamilNadu", "India"));
+		address.put( 191203, new Address("Srinagar","Kasmir", "India"));
+
+		return address.get(zipcode);
+
+	}
+}
